c6f6f921de0ba5e1175867595ad798ec696093fc
Rob Griffiths <rob@bytespider.eu>
2011-02-18 19:39:42 +0000

    Closure

---
+++ src/pipeline.js
@@ -0,0 +1,10 @@
+/**
+ *  @license
+ *  Pipeline version @VERSION
+ *  Copyright (c) 2010 Rob Griffiths (http://bytespider.eu)
+ *  Pipeline is freely distributable under the terms of an MIT-style license.
+ */
+
+var Pipeline = (function(){
+	
+})();
