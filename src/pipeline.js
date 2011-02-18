16f2cf30d643a054e8a66ba64b534c0658575f85
Rob Griffiths <rob@bytespider.eu>
2011-02-18 21:03:59 +0000

    Mutate Native Array into my own array-like object

--- src/pipeline.js
+++ src/pipeline.js
@@ -8,3 +8,33 @@
 var Pipeline = (function(){
 	
 })();
+
+function obj() {
+}
+obj.prototype = new Array();
+
+var prototype = {
+	constructor: obj,
+	toString: function () {return Array.prototype.join()},
+	join: undefined,
+	push: undefined,
+	pop: undefined,
+	concat: undefined,
+	splice: undefined,
+	shift: undefined,
+	unshift: undefined,
+	reverse: undefined,
+	every: undefined,
+	map: undefined,
+	some: undefined,
+	reduce: undefined,
+	reduceRight: undefined,
+};
+
+for (var i in prototype) {
+	obj.prototype[i] = prototype[i];
+}
+
+var obj1 = new obj;
+console.log(obj1);
+
