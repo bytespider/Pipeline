f7fd61ee9b90d14cd7a9506f797b6443e028cc45
Rob Griffiths <rob@bytespider.eu>
2011-02-19 15:16:41 +0000

    DocumentCollection::Find() returns a DocumentCollectionCursor with ALL records

--- spec/suites/BaseSpec.js
+++ spec/suites/BaseSpec.js
@@ -1,21 +1,44 @@
 test('Basics', function () {
-    ok(Pipeline !== undefined, 'Pipeline Constructor exists exists');
+    ok(Pipeline !== undefined, 'Pipeline Constructor exists');
 
 	var db = new Pipeline();
-    ok(db instanceof Pipeline, 'Pipeline returns a instance');
+    ok(db instanceof Pipeline, 'new Pipeline returns a instance');
     
     equals(db.users, undefined, 'The database has no users collection');
     
 	var collection = db.createCollection('users'); // create a collection
 	
-    
-    ok(collection instanceof Object, 'Pipeline::createCollection returns a collection');
-    
+    ok(collection instanceof DocumentCollection, 'Pipeline::createCollection() returns a collection');
+    ok(db.users instanceof DocumentCollection, 'The database has a users collection');
     
     equals(typeof collection.find, 'function', 'DocumentCollection has a find function');
     equals(typeof collection.insert, 'function', 'DocumentCollection has a insert function');
     equals(typeof collection.update, 'function', 'DocumentCollection has a update function');
     equals(typeof collection.save, 'function', 'DocumentCollection has a save function');
     equals(typeof collection.remove, 'function', 'DocumentCollection has a remove function');
+    equals(typeof collection.length, 'number', 'DocumentCollection has a length property');
     
+    collection.insert({
+    	name: 'Darth Vader',
+    	age: 43
+    });
+    equals(collection.length, 1, 'DocumentCollection has 1 new record after insert');
+    
+    collection.insert({
+    	name: 'Leia Organa Solo',
+    	age: 21
+    });
+    collection.insert({
+    	name: 'Luke Skywalke',
+    	age: 21
+    },{
+    	name: 'Yoda',
+    	age: 899
+    });
+    equals(collection.length, 4, 'DocumentCollection has 4 records');
+    
+    var cursor = db.users.find();
+    ok(cursor instanceof DocumentCollectionCursor, 'DocumentCollection::find() returns a cursor');
+
+	    
 });
--- src/pipeline.js
+++ src/pipeline.js
@@ -5,26 +5,43 @@
  *  Pipeline is freely distributable under the terms of an MIT-style license.
  */
 
-(function(){
+//(function(){
+	var dataStore = {}, ArrayProto = Array.prototype;
+	
 	function Pipeline() {}
 	
 	Pipeline.prototype = {
 		createCollection: function (collection) {
-			this[collection] = new DocumentCollection();
+			dataStore[collection] = [];
+			this[collection] = new DocumentCollection(collection);
 			return this[collection];
 		}
 	};
 	
-	function DocumentCollection() {}
+	function DocumentCollection(collection) {
+		this.name = collection;
+	}
 	DocumentCollection.prototype = {
-		find: function () {},
-		insert: function () {},
+		length: 0,
+		find: function () {
+			if (arguments.length == 0) {
+				return new DocumentCollectionCursor(dataStore[this.name]);
+			}
+		},
+		insert: function () {
+			var records = ArrayProto.slice.call(arguments, 0);
+			ArrayProto.push.apply(dataStore[this.name], records);
+			this.length = dataStore[this.name].length;
+			return this;
+		},
 		update: function () {},
 		save: function () {},
 		remove: function () {},
 	};
 	
-	function DocumentCollectionCursor() {}
+	function DocumentCollectionCursor(array) {
+		ArrayProto.push.apply(this, array);
+	}
 	DocumentCollectionCursor.prototype = [];
 	 
 	var extend = {
@@ -50,4 +67,4 @@
 	}
 	
 	window['Pipeline'] =  Pipeline;
-})();
+//})();
