dd096a1a84b6414caae0529935f96c515a3d01da
Rob Griffiths <rob@bytespider.eu>
2011-02-24 16:11:12 +0000

    Added persistence with DOM LocalStorage API

--- spec/suites/BaseSpec.js
+++ spec/suites/BaseSpec.js
@@ -4,62 +4,97 @@ test('Basics', function () {
 	var db = new Pipeline();
     ok(db instanceof Pipeline, 'new Pipeline returns a instance');
     
-    equals(db.users, undefined, 'The database has no users collection');
+    ok(!(db.users instanceof DocumentCollection), 'The database has no users collection');
     
 	var collection = db.createCollection('users'); // create a collection
 	
     ok(collection instanceof DocumentCollection, 'Pipeline::createCollection() returns a collection');
     ok(db.users instanceof DocumentCollection, 'The database has a users collection');
+    console.log(collection);
     
     equals(typeof collection.find, 'function', 'DocumentCollection has a find function');
-    equals(typeof collection.insert, 'function', 'DocumentCollection has a insert function');
-    equals(typeof collection.update, 'function', 'DocumentCollection has a update function');
-    equals(typeof collection.save, 'function', 'DocumentCollection has a save function');
-    equals(typeof collection.remove, 'function', 'DocumentCollection has a remove function');
-    equals(typeof collection.length, 'number', 'DocumentCollection has a length property');
-    
-    collection.insert({
-    	name: 'Darth Vader',
-    	age: 43
-    });
-    equals(collection.length, 1, 'DocumentCollection has 1 new record after insert');
-    
-    collection.insert({
-    	name: 'Leia Organa Solo',
-    	age: 21
-    });
-    collection.insert({
-    	name: 'Luke Skywalker',
-    	age: 21
-    },{
-    	name: 'Yoda',
-    	age: 899
-    });
-    equals(collection.length, 4, 'DocumentCollection has 4 records');
-    
-    var cursor1 = db.users.find();
-    ok(cursor1 instanceof DocumentCollectionCursor, 'DocumentCollection::find() returns a cursor');
-    console.log(cursor1);
+	equals(typeof collection.insert, 'function', 'DocumentCollection has a insert function');
+	equals(typeof collection.update, 'function', 'DocumentCollection has a update function');
+	equals(typeof collection.save, 'function', 'DocumentCollection has a save function');
+	equals(typeof collection.remove, 'function', 'DocumentCollection has a remove function');
+	equals(typeof collection.length, 'number', 'DocumentCollection has a length property');
+	
+	
+	collection.insert({
+		name: 'Darth Vader',
+		age: 43
+	});
+	equals(collection.length, 1, 'DocumentCollection has 1 new record after insert');
+	
+	collection.insert({
+		name: 'Leia Organa Solo',
+		age: 21
+	});
+	collection.insert({
+		name: 'Luke Skywalker',
+		age: 21
+	},{
+		name: 'Yoda',
+		age: 899
+	});
+	equals(collection.length, 4, 'DocumentCollection has 4 records');
+	
+	var cursor1 = db.users.find();
+	ok(cursor1 instanceof DocumentCollectionCursor, 'DocumentCollection::find() returns a cursor');
+	console.log(cursor1);
 	
 	var cursor2 = db.users.find(function() {return age > 21;});
 	equals(cursor2.length, 2, 'DocumentCollection::find() found 2 results using function query');
-    console.log(cursor2);
+	console.log(cursor2);
 	
 	var cursor3 = db.users.find({age: 21, name: 'Luke Skywalker'});
 	equals(cursor3.length, 1, 'DocumentCollection::find() found 2 results using JSON query');
-    console.log(cursor3);
-    
+	console.log(cursor3);
+	
 	var cursor4 = db.users.find('name == "Luke Skywalker"');
 	equals(cursor4.length, 1, 'DocumentCollection::find() found 2 results using string query');
-    console.log(cursor4);
-    
+	console.log(cursor4);
+	
 	db.users.update({age: 43}, {name: 'Anakin Skywalker'});
 	var cursor5 = db.users.find({_id: 0});
 	equals(cursor5[0].name, 'Anakin Skywalker', 'DocumentCollection::update() updated record 0 with new name "Anakin Skywalker"');
-    console.log(cursor5);
-    
+	console.log(cursor5);
+	
 	db.users.remove(function () {return age > 800});
 	var cursor6 = db.users.find();
 	equals(cursor6.length, 3, 'DocumentCollection::remove() removed 1 record');
-    console.log(cursor6);
+	console.log(cursor6);
+	
+	db.dropCollection('users');
+	
+	//Persistence test
+	if (!db.episodes) {
+		var collection = db.createCollection('episodes'); // create a collection
+		console.log(collection);
+		collection.insert({
+			name: 'Star Wars Episode I: The Phantom Menace',
+			year: 1999
+		},{
+			name: 'Star Wars Episode II: Attack of the Clones',
+			year: 2002
+		},{
+			name: 'Star Wars Episode III: Revenge of the Sith',
+			year: 2005
+		},{
+			name: 'Star Wars Episode IV: A New Hope',
+			year: 1977
+		},{
+			name: 'Star Wars Episode V: The Empire Strikes Back',
+			year: 1980
+		},{
+			name: 'Star Wars Episode VI: Return of the Jedi',
+			year: 1983
+		});
+		
+		alert('Inserted ' + collection.length + ' records for persistence test, Please refresh to complete');
+		
+	} else {
+		ok(db.episodes instanceof DocumentCollection, 'The database persisted episodes collection');
+	}
+	
 });
--- src/pipeline.js
+++ src/pipeline.js
@@ -1,23 +1,78 @@
 	
-	var adapter, dataStore = {}, ArrayProto = Array.prototype;
+	var dataStore = {}, ArrayProto = Array.prototype;
 	
-	function Pipeline() {}
+	function Pipeline(config) {
+		var dbName, adapter, itemPrefix, jsonstring, collectionName;
+		
+		config || (config = {});
+		dbName = config.name || 'default';
+		
+		dataStore[dbName] = {}; // initialise global datastore with new database
+	
+		adapter = config.adapter || window.localStorage;
+		
+		this.saveDatabase = function () {
+			for (var collectionName in dataStore[dbName]) {
+				adapter.setItem(dbName + '.' + collectionName, JSON.stringify(dataStore[dbName][collectionName]));
+			}
+		};
+		
+		this.createCollection = function (collection) {
+			return Pipeline.prototype.createCollection.call(this, collection, dbName);
+		};
+		
+		this.dropCollection = function (collection) {
+			Pipeline.prototype.dropCollection.call(this, collection, dbName);
+			adapter.removeItem(dbName + '.' + collection);
+		};
+		
+		// load from the adapter
+		for (var i in adapter) {
+			itemPrefix = dbName + '.';
+			if (i.indexOf(itemPrefix) === 0) {
+				jsonstring = adapter.getItem(i);
+				collectionName = i.replace(itemPrefix, '');
+				if (jsonstring) {
+					dataStore[dbName][collectionName] = JSON.parse(adapter.getItem(i));
+					this.createCollection(collectionName, dbName);
+				}
+			}
+		}
+	}
 	
 	Pipeline.prototype = {
-		createCollection: function (collection) {
-			dataStore[collection] = [];
-			this[collection] = new DocumentCollection(collection);
+		createCollection: function (collection, dbName) {
+			if (collection in this) {
+				return this[collection];
+			}
+			
+			if (!(collection in dataStore[dbName])) {
+				dataStore[dbName][collection] = [];
+			}
+			this[collection] = new DocumentCollection(collection, dbName);
+			this[collection].saveDatabase = this.saveDatabase;
+			
+			this.saveDatabase();
+			
 			return this[collection];
+		},
+		dropCollection: function (collection, dbName) {
+			if (collection in this) {
+				delete dataStore[dbName][collection];
+				delete this[collection];
+			}
+			this.saveDatabase();
 		}
 	};
 	
-	function DocumentCollection(collection) {
+	function DocumentCollection(collection, dbName) {
 		this.name = collection;
+		this.dbName = dbName;
 	}
 	DocumentCollection.prototype = {
 		length: 0,
 		find: function (query) {
-			var results = dataStore[this.name], fn, fnBody, filter, i;
+			var results = dataStore[this.dbName][this.name], fn, fnBody, filter, i;
 
 			if (typeof query == 'object') {
 				fn = [];
@@ -43,20 +98,22 @@
 			
 			if (fnBody) {
 				filter = Function.prototype.constructor.apply(this, ['element', 'with (element) {' + fnBody + '}']);
-				results = dataStore[this.name].filter(filter);
+				results = dataStore[this.dbName][this.name].filter(filter);
 			}
 			
 			return new DocumentCollectionCursor(results);
 		},
-		insert: function (document) {
+		insert: function (newObj) {
 			var records = ArrayProto.slice.call(arguments, 0), i = 0, len = records.length;
 			for (; i < len; ++i) {
 				if (!('_id' in records[i])) {
-					records[i]._id = dataStore[this.name].length;
+					records[i]._id = dataStore[this.dbName][this.name].length;
 				}
-				ArrayProto.push.apply(dataStore[this.name], [records[i]]);
+				ArrayProto.push.apply(dataStore[this.dbName][this.name], [records[i]]);
 			}
-			this.length = dataStore[this.name].length;
+			this.length = dataStore[this.dbName][this.name].length;
+			this.saveDatabase();
+			
 			return this;
 		},
 		update: function (criteria, newObj) {
@@ -66,21 +123,25 @@
 					records[i][j] = newObj[j];
 				}
 			}
+			this.saveDatabase();
+			
 			return this;
 		},
 		save: function () {},
 		remove: function (criteria) {
 			var records = this.find(criteria), i = 0, j, len = records.length, index;
 			for (; i < len; ++i) {
-				index = dataStore[this.name].indexOf(records[i]);
+				index = dataStore[this.dbName][this.name].indexOf(records[i]);
 				if (index > -1) {
-					delete dataStore[this.name][index];
+					delete dataStore[this.dbName][this.name][index];
 				};
 			}
 
-			dataStore[this.name] = dataStore[this.name].filter(function (e) {return e !== undefined;});
+			dataStore[this.dbName][this.name] = dataStore[this.dbName][this.name].filter(function (e) {return e !== undefined;});
 			
-			this.length = dataStore[this.name].length;
+			this.length = dataStore[this.dbName][this.name].length;
+			this.saveDatabase();
+			
 			return this;
 		},
 	};
